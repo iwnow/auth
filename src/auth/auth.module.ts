ef044a75349b8e981a764a198ecfcef626111df4
Vostroknutov Vitaliy <vostroknutov@hotmail.com>
2018-03-27 23:52:56 +0300

    feat(validation+secure): добавил валидацию для signUp + добавил helmet (защита)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewaresConsumer } from '@nestjs/common';
 import { SignController } from './controllers';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
 
 @Module({
 	components: [],
 	controllers: [SignController],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+	configure(consumer: MiddlewaresConsumer) {
+		HelmetMiddleware.configure({});
+		return consumer.apply(HelmetMiddleware)
+			.forRoutes(SignController);
+	}
+}
--- src/auth/controllers/sign.controller.ts
+++ src/auth/controllers/sign.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
 import { SignUpDto } from '../dto';
+import {SignUpValidationPipe} from '../pipes';
 
 @Controller('api/sign')
 export class SignController {
 
+	/** create new user */
 	@Post('up')
+	@UsePipes(new SignUpValidationPipe())
 	signUp(@Body() signUpDto: SignUpDto) {
-		return signUpDto;
+		return 'User created!';
 	}
 }
