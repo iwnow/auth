1c49bbbce90a5a75780b5d98cc0125bdeef3dfe9
Vostroknutov Vitaliy <vostroknutov@hotmail.com>
2018-03-28 00:08:12 +0300

    feat(sign): добавил signIn, signOut http методы

--- src/auth/controllers/sign.controller.ts
+++ src/auth/controllers/sign.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, HttpCode } from '@nestjs/common';
 import { SignUpDto } from '../dto';
 import {SignUpValidationPipe} from '../pipes';
 
@@ -10,5 +10,21 @@ export class SignController {
 	@UsePipes(new SignUpValidationPipe())
 	signUp(@Body() signUpDto: SignUpDto) {
 		return 'User created!';
+	}
+
+	/** signIn/login to app
+	 * this method create and return new token
+	 * if user or mobile device is exist/signUp
+	 */
+	@Post('in')
+	signIn(@Body() signInDto) {
+		return 'New Token';
+	}
+
+	/** forced invalidate token, exit from app */
+	@Post('out')
+	@HttpCode(200)
+	signOut(@Body() signOutDto) {
+
 	}
 }
